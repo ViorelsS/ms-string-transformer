10361d9a8512b9b90d7f1308b40262e2aa66eea5
Viorel Strogoteanu <viorelss@icloud.com>
2024-08-19 11:12:07 +0200

    init

---
+++ src/main/java/it/dedagroup/academy/ms_string_transformer/MsStringTransformerApplication.java
@@ -0,0 +1,13 @@
+package it.dedagroup.academy.ms_string_transformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsStringTransformerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsStringTransformerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/dedagroup/academy/ms_string_transformer/MsStringTransformerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.dedagroup.academy.ms_string_transformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsStringTransformerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
